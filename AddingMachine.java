d08d56f6f0dfd6a7a64e21dcd82802f5a15e5e3f
GabrielCoding98 <gabrielgulbranson@gmail.com>
2019-10-08 17:56:40 -0700

    Added a copy of the AddingMachine.java file into the repository and added comments into it

---
+++ AddingMachine.java
@@ -0,0 +1,37 @@
+//Name: Gabriel Gulbranson
+//ID: 1212824845
+//Course: CSE 360
+//Assignment: 2
+//Description: Adding Machine does basic ariithmetic operations
+//             such as add and substract. Other methods like the toString method
+//             returns the history for which methods were used.
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
